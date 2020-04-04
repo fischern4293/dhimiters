37f92cdc31e6bd1d2e6417f753e133ce39994227
ndf <fischern4293@uwec.edu>
2020-04-03 21:56:28 -0500

    send4

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function initMap(){
-  //location of Dhimiters
-  var dhimiters = 
-}
+
+
+
+const menuButton = document.getElementById('menu-button');
+const navigation = document.getElementById('navigation');
+menuButton.addEventListener('click', event => {
+  navigation.classList.toggle('open');
+  menuButton.classList.toggle('active');
+  event.stopPropagation();
+
+})
+
+document.addEventListener('click', () => {
+  navigation.classList.remove('open');
+  menuButton.classList.remove('active');
+
+})
